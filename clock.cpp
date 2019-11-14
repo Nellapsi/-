5db68049ccf1195e031dda63cb82fcf150173904
Кирилл <katochimoto95@mail.ru>
2019-11-14 13:20:59 +0500

    новые задачи

--- clock.cpp
+++ clock.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 #include <string>
 
-void clock (int n) {
-    int i, j, k;
-    for (i = 0; i <= (n - 1) ; i += 2) {
-        std::cout<<std::endl;
-        for(k = 0; k <= (i + 2) / 2; k++) {
+void clock (int n, int z) {
+    int i, j, k, q;
+    for (i = n; i >= 1; i -= 2) {
+        for(k = 0; k < (n - i) / 2 + z; k++) {
             std::cout <<" ";
         }
-        for(j = i; j<=(n - 1); j++){
+        for(j = 0; j < i; j++){
             std::cout <<"*";
         }
+        std::cout<<std::endl;
     }
     for(i = 3; i <= n; i += 2) {
         std::cout<<std::endl;
+        for (q = z; q >= 0; q--) {
+            std::cout << " ";
+        }
         for(k = n / 2; k >= (i - 2) / 2; k--) {
             std::cout<<" ";
         }
@@ -24,13 +27,16 @@ void clock (int n) {
 }
 
 int main () {
-    int n;
+    int n, z;
+    z = 0;
     std::cin >> n;
     for (int i = n; i >= 1; i -= 2) {
-        clock (i);
+        clock (i, z);
+        z++;
     }
     for (int i = 3; i <= n ; i += 2){
-        clock (i);
+        z--;
+        clock (i, z);
     }
     return 0;
 }
--- for32.cpp
+++ for32.cpp
@@ -6,12 +6,12 @@ int main () {
     k = 0;
     c = 1;
     a = 0;
-    if (n <= 0) {
-        while (n <= 0) {
-            std::cout << "vvedite pravilnoe chislo" << std::endl;
-            std::cin >> n;
-        }
+
+    while (n <= 0) {
+        std::cout << "vvedite pravilnoe chislo" << std::endl;
+        std::cin >> n;
     }
+
     for (i = 1; i <= n; i++) { 
         a = c + k;
         c = k;
