3f40c7bf1be4f3f16d48852ea94645c4fc980056
Кирилл <katochimoto95@mail.ru>
2019-11-23 16:11:56 +0500

    123

---
+++ masiv2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main () {
+    std::srand (time (nullptr));
+    int b, n, i, k;
+    k = 0;
+    std::cin >> b;
+    std::vector <int> a (b);
+    for (i = 0; i < b; i++) {  
+        a [i] = std::rand() % (10 + 5 + 1) - 5;
+        std::cout << i << ". " << a[i] << std::endl;
+    }
+    std::cin >> n;
+    for(int j = 0; j < b; j++) {
+        if (n == a[j]){
+            std::cout << "pervi raz chiclo bilo v " << j << " elemente" << std::endl;
+            k = 1;
+            break;
+        }
+    }
+    if (k == 0) {
+        std::cout << "takogo chisla net" << std::endl;
+    }
+    return 0;
+}
