fa3d7489b07457e8ffac13583618077a386e01cd
Кирилл <katochimoto95@mail.ru>
2019-11-27 19:38:18 +0500

    123

--- massivs.cpp
+++ massivs.cpp
@@ -6,23 +6,23 @@
 
 int main () {
     std::srand (time (nullptr));
-    int b, c, t;
-    c = 0;
-    t = 1;
+    int b, c;
+    bool d = false;
     std::cin >> b;
-    std::vector <int> a(b);
+    std::vector <int> a (b);
     for (int i = 0; i < b; i++) {
-        a [i] = std::rand() % (10 + 5 + 1) - 5;
+        a [i] = std::rand() % (10 - 1 + 1) + 1;
     }
 
-    while (c != 0) {
-        for(int i = 1; i< b; i++){
+    while (!d) {
+        d = true;
+        for(int i = 1; i < b; i++){
             if (a [i] < a [i -1]) {
                 c = a [i];
                 a [i] = a [i - 1];
                 a [i - 1] = c;
-                t++;
-            } 
+                d = false;
+            }
         }
     }   
     for (int i = 0; i < b; i++) {
