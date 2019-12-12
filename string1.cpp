960d0c66851cf28b9a6ad564e38df1c8c9588cde
Кирилл <katochimoto95@mail.ru>
2019-12-12 19:51:00 +0500

    123

--- string1.cpp
+++ string1.cpp
@@ -2,28 +2,20 @@
 #include <string> 
 
 int main () {
+    std::string b;
     std::string a;
     std::cin >> a;
-    std::string b;
-    bool c = true;
-    for (int i = 0; i < a.size (); i++) {
-        int j = a.size () - i - 1;
-        if (a [i] != 'q') {
-            b.push_back (a [j]);
+    int k = a.size ();
+    for (int i = 0; i < k; i++ ) {
+        if (a [i] != ' ') {
+            b [i] = a [i];
+            std::cout << "a";
+        } else {
+            k--;
         }
     }
-    std::cout << b << std::endl;
     for (int i = 0; i < b.size (); i++) {
-        int j = b.size () - i - 1;
-        std::cout << b [j] << ". " << b [i] << std::endl;
-        if (b [j] != b [i]) {
-                std::cout << "net" << std::endl;
-                c = false;
-                break;
-        }
-    }
-    if (c) {
-        std::cout << "polindrom: " << a << std::endl;
+        std::cout << b [i];
     }
     return 0;
 }   
